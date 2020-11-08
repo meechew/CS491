7c39fc02a326168e5e6aa7816a5e44cb549e679b
Carl Bunt <cbunt@pdx.edu>
2020-11-08 02:00:27 -0800

    AddedBufferOverFlow.c, but still needs to be writen

---
+++ BufferOverFlow.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+int16_t SHIRT_VAL = 20;
+
+
+int main(int argc, char *argv[]) {
+  if(argc < 2) {
+    printf("usage: UnderFlow <Shirt count>\n");
+    exit(EXIT_FAILURE);
+  }
+  int16_t count = atoi(argv[1]);
+  int16_t Val = SHIRT_VAL * (count * (-1));
+
+  
+  printf("Total: %d\n", Val);
+
+
+  return 0;
+}
+  
