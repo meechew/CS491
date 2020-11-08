2061111b6e76121e4ae5d83d2bf024f9b43e8f2b
Carl Bunt <cbunt@pdx.edu>
2020-11-08 00:59:27 -0800

    Added OverFlow.c

---
+++ OverFlow.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+uint16_t SHIRT_VAL = 20;
+
+
+int main(int argc, char *argv[]) {
+  if(argc < 2) {
+    printf("usage: OverFlow <Shirt count>\n");
+    exit(EXIT_FAILURE);
+  }
+  uint16_t count = atoi(argv[1]);
+  uint16_t Val = SHIRT_VAL * count;
+
+  
+  printf("Total: %d\n", Val);
+
+
+  return 0;
+}
+  
